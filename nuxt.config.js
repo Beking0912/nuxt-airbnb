3779bb669a30f1de19a9ce52d7d62e44773bbce3
Beking0912 <sbqoo@qq.com>
2023-05-09 12:16:53 -0400

    ADD: first page

---
+++ nuxt.config.js
@@ -0,0 +1,17 @@
+export default {
+  components: true,
+  head: {
+    titleTemplate: "nuxt-app: %s",
+    htmlAttrs: {
+      lang: "en",
+    },
+    bodyAttrs: {
+      class: ["my-style"],
+    },
+    meta: [
+      {
+        charset: "utf-8",
+      },
+    ],
+  },
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,30 @@
-<template><div>hello</div></template>
+<template>
+  <div>
+    <div v-for="home in homes" :key="home.objectID">
+      <home-card :home="home" />
+    </div>
+  </div>
+</template>
+
+<script>
+import homes from "~/data/homes";
+export default {
+  head() {
+    return {
+      title: "Homepage",
+      meta: [
+        {
+          name: "description",
+          content: "The homepage of our site.",
+          hid: "description",
+        },
+      ],
+    };
+  },
+  data() {
+    return {
+      homes: homes.slice(0, 3),
+    };
+  },
+};
+</script>
