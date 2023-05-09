82881f00853ba9f7bc5a04de9e259c51dc450590
Beking0912 <sbqoo@qq.com>
2023-05-09 19:08:40 -0400

    ADD: error page

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{ error.message }} {{ error.statusCode }}</div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: "Error",
+    };
+  },
+  props: {
+    error: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
