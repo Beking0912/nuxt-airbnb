80035123137c61b2da8be2c3d8961420ffb216f8
Beking0912 <sbqoo@qq.com>
2023-05-22 14:31:56 -0400

    ADD: gmap autoComplete

--- plugins/maps.client.js
+++ plugins/maps.client.js
@@ -1,10 +1,11 @@
 export default function (content, inject) {
-  let mapLoaded = false;
-  let mapWaiting = null;
+  let isLoaded = false;
+  let waiting = [];
 
   addScript();
   inject("maps", {
     showMap,
+    makeAutoComplete
   });
 
   function addScript() {
@@ -12,26 +13,45 @@ export default function (content, inject) {
     script.async = true;
     script.src =
       "https://maps.googleapis.com/maps/api/js?key=mykey&libraries=places";
-    window.initMap = initMap;
+    window.initGoogleMaps = initGoogleMaps;
     document.head.appendChild(script);
   }
 
-  function initMap() {
-    mapLoaded = true;
-    if (mapWaiting) {
-      const { canvas, lat, lng } = mapWaiting;
-      renderMap(canvas, lat, lng);
-      mapWaiting = null;
+  function initGoogleMaps() {
+    isLoaded = true;
+    waiting.forEach((item) => {
+      if (typeof item.fn === "function") {
+        item.fn(...item.args);
+      }
+    });
+    waiting = [];
+  }
+
+  function makeAutoComplete(input) {  
+    if (!isLoaded) {
+      waiting.push({
+        fn: makeAutoComplete,
+        args,
+      })
+      return
     }
+    const autoComplete = new window.google.maps.places.Autocomplete(input, {
+      types: ["(cities)"],
+    });
+    autoComplete.addListener("place_changed", () => {
+      const place = autoComplete.getPlace();
+      input.dispatchEvent(new CustomEvent("changed", { detail: place }));
+    });
   }
 
   function showMap(canvas, lat, lng) {
-    if (mapLoaded) {
-      renderMap(canvas, lat, lng);
-    } else mapWaiting = { canvas, lat, lng };
-  }
-
-  function renderMap(canvas, lat, lng) {
+    if (!isLoaded) {
+      waiting.push({
+        fn: showMap,
+        args,
+      })
+      return
+    }
     const mapOptions = {
       zoom: 18,
       center: new google.maps.LatLng(lat, lng),
