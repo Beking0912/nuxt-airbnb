9919f27c7c5fdb29c235b6ada7677278dc439e73
Beking0912 <sbqoo@qq.com>
2023-05-23 12:49:15 -0400

    ADD: storing credentials

--- plugins/auth.client.js
+++ plugins/auth.client.js
@@ -16,8 +16,28 @@ export default ({ $config }) => {
             const auth2 = await window.gapi.auth2.init({
                 client_id: $config.auth.clientId,
             });
+
+            auth2.currentUser.listen(parseUser);
         });
 
-        window.gapi.signin2.render('googleButton')
+        window.gapi.signin2.render('googleButton', {
+            onsuccess: parseUser
+        })
+    }
+
+    function parseUser(user) {
+        const profile = user.getBasicProfile();
+        console.log('name:', profile.getName());
+        if (!user.isSignedOut()) {
+            Cookie.remove($config.auth.cookieName);
+            return
+        }
+        const idToken = user.getAuthResponse().id_token;
+        Cookies.set($config.auth.cookieName, idToken, { expires: 1/24, sameSite: 'Lax' });
+    }
+
+    function signOut() {
+        const auth2 = window.gapi.auth2.getAuthInstance();
+        auth2.signOut();
     }
 }
