21e24ba4b6ce1ad31d377d434d4e250212b41000
Beking0912 <sbqoo@qq.com>
2023-05-25 14:29:30 -0400

    ADD: vercel deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 export default {
+  rootUrl: process.env.NODE_ENV === 'production' ? 'https://myapp.vercel.com' : 'http://localhost:3000',
   components: true,
   head: {
     titleTemplate: "nuxt-app: %s",
