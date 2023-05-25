b8e35d1a2153d2b1dd456ac2fdb540acf11056c9
Beking0912 <sbqoo@qq.com>
2023-05-25 14:40:21 -0400

    ADD: stripe webhooks

--- modules/algolia/apis/user.js
+++ modules/algolia/apis/user.js
@@ -32,6 +32,27 @@ export default (algoliaConfig) => {
         return getErrorResponse(error);
       }
     },
+    bookHome: async (identityId, homeId, start, end) => {
+      try {
+        return unWrap(
+          await fetch(
+            `https://${algoliaConfig.appId}-dsn.algolia.net/1/indexes/bookings/`,
+            {
+              headers,
+              method: "POST",
+              body: JSON.stringify({
+                identityId,
+                homeId,
+                start,
+                end,
+              }),
+            }
+          )
+        );
+      } catch (error) {
+        return getErrorResponse(error);
+      }
+    },
     getById: async (identity) => {
       try {
         return unWrap(
